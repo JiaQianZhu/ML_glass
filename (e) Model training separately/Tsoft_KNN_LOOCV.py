410f424a38fa78b5355ce154a5f10fe351b888e0
JiaQianZhu <142502102+JiaQianZhu@users.noreply.github.com>
2023-10-18 11:30:16 +0800

    Add files via upload

---
+++ (e) Model training separately/Tsoft_KNN_LOOCV.py
@@ -0,0 +1,69 @@
+"""
+Author:JiaQian Zhu
+Date:2023-8-17
+Usage:This module offers method to filter out the non-missing portions of Tsoft and conduct training using the KNN algorithm with LOOCV.
+"""
+import pickle
+import pandas as pd
+from sklearn.neighbors import KNeighborsRegressor
+
+data = pd.read_csv('../(a-d) Datasets/raw/data.csv')
+############################################################################
+#                               LOOCV + KNN                                 #
+############################################################################
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error, median_absolute_error, r2_score
+import numpy as np
+
+# Filter out the non-missing data points.
+non_empty_df = data.dropna(subset=['Tsoft'])
+
+# Create an empty list to store the predictions for each sample.
+all_predictions = []
+
+# Create an empty list to store the actual values for each sample.
+all_targets = []
+
+# Use the leave-one-out cross-validation method.
+for index, row in non_empty_df.iterrows():
+    # Select the current sample from the dataset as the testing sample.
+    test_features = row[['SiO2', 'Al2O3', 'CaO', 'MgO', 'SrO', 'B2O3', 'Sb2O3']].values.reshape(1, -1)
+    test_target = row['Tsoft']
+
+    # Exclude the current sample from the dataset to use it as a training sample.
+    train_features = non_empty_df.drop(index)[['SiO2', 'Al2O3', 'CaO', 'MgO', 'SrO', 'B2O3', 'Sb2O3']]
+    train_target = non_empty_df.drop(index)['Tsoft']
+
+    # Create a KNN regression model.
+    knn_model = KNeighborsRegressor()
+
+    # Fit the model.
+    knn_model.fit(train_features, train_target)
+
+    # Make predictions.
+    prediction = knn_model.predict(test_features)
+
+    # Store the predicted results and the actual values.
+    all_predictions.append(prediction)
+    all_targets.append(test_target)
+
+# Convert the predicted results and actual values into NumPy arrays.
+all_predictions = np.array(all_predictions)
+all_targets = np.array(all_targets)
+
+# Calculate evaluation metrics.
+mse = mean_squared_error(all_targets, all_predictions)
+mae = mean_absolute_error(all_targets, all_predictions)
+medae = median_absolute_error(all_targets, all_predictions)
+rd = np.mean(np.abs((all_targets - all_predictions) / all_targets)) * 100
+r2 = r2_score(all_targets, all_predictions)
+
+print("Mean Squared Error:", mse)
+print("Mean Absolute Error:", mae)
+print("Median Absolute Error:", medae)
+print("Relative Difference (%):", rd)
+print("R^2 Score:", r2)
+
+# Save the model as a pickle file.
+with open('../model/individual_prediction/Tsoft_KNN_model.pickle', 'wb') as file:
+    pickle.dump(knn_model, file)
